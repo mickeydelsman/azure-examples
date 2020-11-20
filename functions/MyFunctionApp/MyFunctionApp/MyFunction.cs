b8375ff904d8b27baa692ef91027f5c957916fc0
c69908 <mickey.delsman@nl.abnamro.com>
2020-11-20 18:07:04 +0100

    added MyFunctionApp to repository

---
+++ functions/MyFunctionApp/MyFunctionApp/MyFunction.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Azure.WebJobs;
+using Microsoft.Azure.WebJobs.Extensions.Http;
+using Microsoft.Build.Framework;
+
+public static class MyFunction
+{
+	[FunctionName("HelloAzure")]
+	public static async Task<IActionResult> Run(
+		[HttpTrigger(AuthorizationLevel.Anonymous, "get", "post", Route = null)] HttpRequest httpRequest)
+	{
+		return (ActionResult) new OkObjectResult($"Hello, Azure");
+	}
+}
